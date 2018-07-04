cf321190ffb5daa2cc21f2158763ff3f59671384
Jan Willamowius <jan@willamowius.de>
2018-07-04 11:52:44 +0200

    initial upload

---
+++ eansearch3.py
@@ -0,0 +1,50 @@
+"""
+A Pyhon class for EAN and ISBN name lookup and validation using the API on ean-search.org.
+
+To use it, you need an API access token from https://www.ean-search.org/ean-database-api.html
+
+Python 3.x version
+"""
+
+import urllib.request
+import json
+
+class EANSearch:
+
+	def __init__(self, token):
+		self.token = token;
+
+	def barcodeLookup(self, ean, lang=1):
+		"""Lookup the product name for a EAN barcode"""
+		contents = urllib.request.urlopen("https://api.ean-search.org/api?token=" + self.token \
+			+ "&op=barcode-lookup&format=json&ean=" + ean + "&lang=" + str(lang)).read().decode("utf-8")
+		data = json.loads(contents)
+		if "error" in data[0]:
+			return None
+		else:
+			return data[0]["name"]
+
+	def verifyChecksum(self, ean):
+		"""verify checksum of an EAN barcode"""
+		contents = urllib.request.urlopen("https://api.ean-search.org/api?token=" + self.token \
+			+ "&op=verify-checksum&format=json&ean=" + ean).read().decode("utf-8")
+		data = json.loads(contents)
+		if "error" in data[0]:
+			return None
+		else:
+			return data[0]["valid"]
+
+	def productSearch(self, name, page=0):
+		"""search for a product name"""
+		contents = urllib.request.urlopen("https://api.ean-search.org/api?token=" + self.token \
+			+ "&op=product-search&format=json&name=" + name + "&page=" + str(page)).read().decode("utf-8")
+		data = json.loads(contents)
+		return data["productlist"]
+
+	def barcodePrefixSearch(self, prefix, page=0):
+		"""search for a prefix of EAN barcodes"""
+		contents = urllib.request.urlopen("https://api.ean-search.org/api?token=" + self.token \
+			+ "&op=barcode-prefix-search&format=json&prefix=" + prefix + "&page=" + str(page)).read().decode("utf-8")
+		data = json.loads(contents)
+		return data["productlist"]
+
---
+++ test.py
@@ -0,0 +1,26 @@
+# Python 2.7
+from eansearch2 import EANSearch
+# Python 3.x
+#from eansearch3 import EANSearch
+
+# API token for ean-search.org account
+# https://www.ean-search.org/ean-database-api.html
+apiToken = "jw123"
+ean = "5099750442227" # Thriller
+#ean = "5099750442228" # error
+
+eansearch = EANSearch(apiToken)
+name = eansearch.barcodeLookup(ean)
+print(ean, " is ", name)
+
+ok = eansearch.verifyChecksum(ean)
+print(ean, " is ", "OK" if ok else "Not OK")
+
+eanList = eansearch.productSearch('iPod');
+for product in eanList:
+	print(product["ean"], " is ", product["name"].encode("utf-8"))
+
+eanList = eansearch.barcodePrefixSearch('4007249146');
+for product in eanList:
+	print(product["ean"], " is ", product["name"].encode("utf-8"))
+
