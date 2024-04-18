9958a6b5606449714029d554c73a6668ab1ff9db
Jan Willamowius <jan@willamowius.de>
2024-04-18 21:12:24 +0200

    add isbnLookup()

--- test.py
+++ test.py
@@ -13,9 +13,13 @@ eansearch = EANSearch(apiToken)
 name = eansearch.barcodeLookup(ean)
 print(ean, " is ", name)
 
-# more detailed result, preferrably in English (1)
+# more detailed result, preferably in English (1)
 product = eansearch.barcodeSearch(ean, 1)
 print(ean, " is ", product["name"].encode("utf-8"), " from category ", product["categoryName"], " issued in ", product["issuingCountry"])
+
+isbn = "1119578884";
+title = eansearch.isbnLookup(isbn)
+print(isbn, " is ", title)
 
 ok = eansearch.verifyChecksum(ean)
 print(ean, " is ", "OK" if ok else "Not OK")
